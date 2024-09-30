0b2ea4874f3feae941021d2e561b446d102fd617
mayurlilhare <mayurlilhare00@gmail.com>
2024-09-30 14:50:48 +0530

    es6ArrayMethods file first commit

---
+++ src/module1/es6ArrayMethods.js
@@ -0,0 +1,47 @@
+export default function ES6ArrayMethods() {
+    const personArray = [
+        {
+            name:"Person1",
+            age:30,
+            country:"India"
+        },
+        {
+            name:"Person4",
+            age:30,
+            country:"India"
+        },
+        {
+            name:"Person2",
+            age:32,
+            country:"China"
+        },
+        {
+            name:"Person3",
+            age:30,
+            country:"Russia"
+        },
+    ];
+
+    // map function
+    let getAllNames = personArray.map((e, index)=>{
+        // console.log(e.name, index)
+        return e.name;
+    })
+
+    console.log("getAllNames",getAllNames)
+
+    // find function = incase of duplicasy returns first found val
+    let getSinglePerson = personArray.find((e)=>{
+        return e.country === "India"
+    })
+
+    console.log("getSinglePerson", getSinglePerson)
+
+
+    // filter = incase of duplicasy returns all found vals
+    let getAllSearch = personArray.filter(e=>
+       e.country.toUpperCase().includes("India".toUpperCase())
+    )
+
+    console.log("getAllSearch", getAllSearch)
+};
